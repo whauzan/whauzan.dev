b6a243d5c67ef0f07a879b8c8af317c71b05720e
whauzan <wahyu172845@gmail.com>
2025-08-21 03:21:38 +0700

    build(commitlint): Add conventional commit message validation

---
+++ commitlint.config.js
@@ -0,0 +1,32 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation changes
+        "style", // Code style changes (formatting, etc)
+        "refactor", // Code refactoring
+        "perf", // Performance improvements
+        "test", // Adding or updating tests
+        "chore", // Maintenance tasks
+        "ci", // CI/CD changes
+        "build", // Build system changes
+        "revert", // Reverting changes
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [2, "always", "sentence-case"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 72],
+    "body-leading-blank": [1, "always"],
+    "body-max-line-length": [2, "always", 100],
+    "footer-leading-blank": [1, "always"],
+  },
+};
